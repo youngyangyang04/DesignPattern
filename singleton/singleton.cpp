dfd501ba4bc408f8ec91609c2e7aa67972ac0046
Carl Sun <826123027@qq.com>
2016-08-08 16:05:33 +0800

    update singleton

--- singleton/singleton.cpp
+++ singleton/singleton.cpp
@@ -22,8 +22,10 @@ private:
 	};
 	static Singleton* _instance;
 };
-Singleton *Singleton::_instance = 0;
+Singleton *Singleton::_instance = 0;//The C++ standard allows only static constant integral or enumeration types to be initialized inside the class.
 int main(){
 	Singleton* sgn1 = Singleton::Instance();
 	Singleton* sgn2 = Singleton::Instance();
+	delete sgn1;
+//	delete sgn2; sgn2 is same with sgn1, so deleting two times will make error
 }
